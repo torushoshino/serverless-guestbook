913280cd37f2e36cdef76944053a7e5956e83185
shuichi hoshino <shuichi_hoshino@torus.co.jp>
2019-07-16 19:08:20 +0900

    apiurlを変更

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,8 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://4ee0a339.us-south.apiconnect.appdomain.cloud/guestbook';
+//const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
